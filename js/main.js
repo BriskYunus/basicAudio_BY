e761f9a262990e703d5b8cb6386caa8cb97b5648
Yunus <b_yunus@tss-hr420-dm37.fc.ca>
2019-02-25 15:45:28 -0500

    added builds

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (() => {
 	// create a variable stack and grab all of the DOM elements from the page
-	let aud = 		document.querySelector('audio'),
-		play = 		document.querySelector('#playButton'),
-		pause = 	document.querySelector('#pauseButton'),
-		rewind = 	document.querySelector('#rewindButton');
+	let aud = document.querySelector('audio'),
+		play = document.querySelector('#playButton'),
+		pause = document.querySelector('#pauseButton'),
+		rewind = document.querySelector('#rewindButton'),
+		tracks = document.querySelectorAll('.trackholder');
 
 	// write the functions for the audio element
 	function playAudio() {
@@ -18,10 +19,20 @@
 		aud.currentTime = 0;
 	}
 
+	function swapSource() {
+		let currentTrack = this.dataset.currenttrack;
+
+		aud.src = `audio/${currentTrack}`;
+		aud.load();
+		aud.play();
+	}
+
 	// add event handling
 	play.addEventListener("click", playAudio);
 	pause.addEventListener("click", pauseAudio);
 	rewind.addEventListener("click", rewindAudio);
+
+	tracks.forEach(track => track.addEventListener("click", swapSource));
 
 
 })();
